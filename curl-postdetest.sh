db192ef85cbe807b3b074a56ff7303db7652e7cd
etiquet <eric.tiquet@gmail.com>
2024-06-22 09:51:55 +0200

    change url

--- curl-postdetest.sh
+++ curl-postdetest.sh
@@ -5,5 +5,4 @@ curl -X POST \
 -H "Content-Encoding: gzip" \
 -H "X-Api-Key: ZHpiwUuiffdectKeJG24cVudQi9VXlYI" \
 --data-binary "@20240615103132_odf.gzip" \
-http://localhost:12000/ODFClient
-# replace above by correct url
+http://localhost:12000/ODFClient # change url
