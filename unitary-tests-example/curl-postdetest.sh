03bdd7ffaf29c72489dad1c20ad8821f36e80517
zkarahacane <z.karahacane@actongroup.com>
2024-07-12 14:04:36 +0200

    refacto for prod

--- unitary-tests-example/curl-postdetest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-curl -X POST \
--H "Content-Type: application/xml; charset=UTF-8" \
--H "Accept-Encoding: gzip" \
--H "Content-Encoding: gzip" \
--H "X-APIkey: d1a3d4cf-b379-426b-ab43-ddc29d7ce74a" \
---data-binary "@20240615103132_odf.gzip" \
-http://localhost:5001/ODFClient
-#attention sur mac le port 5000 est utilisé > passer à 5001 
